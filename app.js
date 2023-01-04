ad613c21ffbaca32bdaa36a96018048ceab5ba44
Uzair <uzair.shayansolutions@gmail.com>
2023-01-04 12:49:59 +0500

    Registerion Post

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
-
 const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 dotenv.config({ path: "./config.env" });
 require("./DB/conn");
-// const User = require("./model/userSchema");
 
+const User = require("./model/userSchema");
+app.use((express.json()));
 // we link the router files to make our route easy
 app.use(require("./router/auth"));
-//jojojdfsf
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 
-//middleware
-const middleware = (req, res, next) => {
-  console.log("Hello from middlware");
-  next();
-};
-
-app.get("/about", middleware, (req, res) => {
-  console.log("Hello from About");
-  res.send(`Hello About from the server`);
-});
-app.get("/contact", (req, res) => {
-  res.send(`Hello Contact from the server`);
-});
-app.get("/sigin", (req, res) => {
-  res.send(`Hello sigin from the server`);
-});
-app.get("/signup", (req, res) => {
-  res.send(`Hello signup from the server`);
-});
 
 app.listen(3000, () => {
   console.log(`server is running on PORT ${PORT}`);
--- router/auth.js
+++ router/auth.js
@@ -1,7 +1,43 @@
 const express= require('express');
 const router = express.Router();
 
+const User = require("../model/userSchema");
+//middleware
+const middleware = (req, res, next) => {
+  console.log("Hello from middlware");
+  next();
+};
 router.get("/", (req, res) => {
     res.send(`Hello from the server`);
   });
+router.get("/about", middleware, (req, res) => {
+  console.log("Hello from About");
+  res.send(`Hello About from the server`);
+});
+router.get("/contact", (req, res) => {
+  res.send(`Hello Contact from the server`);
+});
+router.get("/login", (req, res) => {
+
+});
+router.post("/register", async(req, res) => {
+  const {name, email, phone, work, password, cpassword} = req.body;
+  if(!name || !email || !phone || !work || !password || !cpassword){
+    return res.status(422).json({error:"Pls filled the field properly"})
+  }
+  try {
+    const userExist = await User.findOne({email:email});
+    if(userExist){
+      return res.status(422).json({error:"Email already exist"});
+    }
+    const user = new User({name, email, phone, work, password, cpassword});
+    await user.save();
+    res.status(201).json({massage:"user registered successfully"});
+
+  } catch (err) {
+    console.log(err);
+  }
+
+});
+
    module.exports = router;
