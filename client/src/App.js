29a5f3f15a66cb7636b29b25d6ba1cf99fc2a99e
uzair <uzair.shayansolutions@gmail.com>
2023-01-05 18:58:38 +0500

    Nav bar and Components Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import NavBar from "./Component/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Component/Home";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
+import Login from "./Component/Login";
+import Singup from "./Component/Singup";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Singup />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
